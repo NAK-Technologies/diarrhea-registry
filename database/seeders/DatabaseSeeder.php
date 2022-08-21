a46394084915197124f6c97b9284d19c270f4a72
Nehal Ahmed Khan <81828354+NAK-Technologies@users.noreply.github.com>
2022-08-21 13:06:00 +0500

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,18 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([UsersTableSeeder::class, CitySeeder::class]);
+        
+        $json = File::get('public/storage/assets/pk.json');
+        // $json = Storage::disk('local')->get(storage_path() . '/assets/pk.json');
+        // $json = File::get($json);
+        $cities = collect(json_decode($json, true));
+        // dd($cities);
+        foreach ($cities as $city) {
+            $new = new City();
+            $new->create([
+                'name' => $city['city'],
+                'state' => $city['admin_name']
+            ]);
+        }
     }
 }
