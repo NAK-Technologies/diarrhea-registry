c4fb54538c4a0659e88bf8c6b896027bfd7768f4
Nehal Ahmed Khan <nak.work.dev@gmail.com>
2022-08-28 13:13:32 +0500

    User seeder for dev

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Database\Seeders;
 
 use Illuminate\Support\Facades\DB;
@@ -6,7 +7,8 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Storage;
 use App\Models\City;
-
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,19 +19,24 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call([UsersTableSeeder::class]);
-        
-        $json = File::get('public/storage/assets/pk.json');
-        // $json = Storage::disk('local')->get(storage_path() . '/assets/pk.json');
-        // $json = File::get($json);
-        $cities = collect(json_decode($json, true));
-        // dd($cities);
-        foreach ($cities as $city) {
-            $new = new City();
-            $new->create([
-                'name' => $city['city'],
-                'state' => $city['admin_name']
-            ]);
-        }
+        // $this->call([UsersTableSeeder::class]);
+
+        // $json = File::get('public/storage/assets/pk.json');
+        // // $json = Storage::disk('local')->get(storage_path() . '/assets/pk.json');
+        // // $json = File::get($json);
+        // $cities = collect(json_decode($json, true));
+        // // dd($cities);
+        // foreach ($cities as $city) {
+        //     $new = new City();
+        //     $new->create([
+        //         'name' => $city['city'],
+        //         'state' => $city['admin_name']
+        //     ]);
+        // }
+        User::factory()->create([
+            'name' => 'Dev',
+            'email' => 'dev@dspss.com',
+            'password' => Hash::make('@secret123'),
+        ]);
     }
 }
