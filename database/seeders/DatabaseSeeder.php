11f2b9699339301ae506e97eda126b35464d9339
Nehal Ahmed Khan <nak.work.dev@gmail.com>
2022-08-28 19:19:42 +0500

    Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,20 +19,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call([UsersTableSeeder::class]);
+        $this->call([UsersTableSeeder::class]);
 
-        // $json = File::get('public/storage/assets/pk.json');
-        // // $json = Storage::disk('local')->get(storage_path() . '/assets/pk.json');
-        // // $json = File::get($json);
-        // $cities = collect(json_decode($json, true));
-        // // dd($cities);
-        // foreach ($cities as $city) {
-        //     $new = new City();
-        //     $new->create([
-        //         'name' => $city['city'],
-        //         'state' => $city['admin_name']
-        //     ]);
-        // }
+        $json = File::get('public/storage/assets/pk.json');
+        // $json = Storage::disk('local')->get(storage_path() . '/assets/pk.json');
+        // $json = File::get($json);
+        $cities = collect(json_decode($json, true));
+        // dd($cities);
+        foreach ($cities as $city) {
+            $new = new City();
+            $new->create([
+                'name' => $city['city'],
+                'state' => $city['admin_name']
+            ]);
+        }
         User::factory()->create([
             'name' => 'Dev',
             'email' => 'dev@dspss.com',
